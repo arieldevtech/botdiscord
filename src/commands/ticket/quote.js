680569e2b71eaa25b9fbc718aa2f21752d39ae84
arieldevtech <arieldev.tech@gmail.com>
2025-08-27 11:02:23 +0200

    Updated quote.js

--- src/commands/ticket/quote.js
+++ src/commands/ticket/quote.js
@@ -7,14 +7,4 @@ function hasAdminRole(member) {
 }
 
 module.exports = {
-    const supportRoleIds = config.supportRoleIds || [];
-    
-    // Vérifier si l'utilisateur a un rôle de support ou admin
-    const hasSupport = supportRoleIds.some(roleId => member.roles.cache.has(roleId));
-    const hasAdmin = hasAdminRole(member);
-    if (!hasSupport && !hasAdmin) {
-      return interaction.reply({
-        ephemeral: true,
-        embeds: [errorEmbed("❌ Vous devez avoir un rôle de support ou administrateur pour utiliser cette commande.")]
-      });
-    }
+};
